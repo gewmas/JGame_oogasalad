32ea0617a681a2b933212a8c48e0a5459abed85d
Harris Osserman <harris.osserman@gmail.com>
2013-11-04 16:59:42 -0500

    Able to get value from JSON file given key

--- src/gameEngine/parser/Parser.java
+++ src/gameEngine/parser/Parser.java
@@ -2,11 +2,25 @@ package gameEngine.parser;
 
 import java.util.Scanner;
 
+import gameEngine.parser.JSONLibrary.*;
+
 public class Parser {
 	private Scanner scanner;
+	private JSONObject jsonObject;
 	public Parser(Scanner scanner) {
 		this.scanner = scanner;
+		jsonObject = new JSONObject(constructJSONString());
 	}
 	
+	public String constructJSONString() {
+		String returnString = "";
+		while(scanner.hasNextLine()) {
+			returnString = returnString + scanner.nextLine();
+		}
+		return returnString;
+	}
 	
+	public String getValue(String key) {
+		return (String) jsonObject.get(key);
+	}
 }
--- src/gameEngine/test/TestParser.java
+++ src/gameEngine/test/TestParser.java
@@ -1,30 +1,43 @@
 package gameEngine.test;
 
 import static org.junit.Assert.*;
-import gameEngine.*
+
+import java.io.File;
+import java.util.Scanner;
+
+import gameEngine.parser.Parser;
+import gameEngine.parser.JSONLibrary.JSONException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestParser {
-
+	private Parser parser;
 	@Before
 	public void setUp() throws Exception {
+		Scanner sc = new Scanner(new File("src/gameEngine/test/TestJSON.json"));
+		parser = new Parser(sc);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetNameOfGame() {
+		String getNameOfGame = parser.getValue("name");
+		assertEquals(getNameOfGame, "Tower Destruction II");
 	}
 	
 	@Test
-	public void testCreateParser() {
-		
+	public void testGetNonexistenAttribute() {
+		try {
+			String getNameOfGame = parser.getValue("totalGold");
+			assert(false);
+		} catch (JSONException e) {
+			assert(true);
+		}
 	}
 
 }
