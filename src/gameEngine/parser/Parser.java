d65aee761d455206168e9b3f40c776ab085a668b
Harris Osserman <harris.osserman@gmail.com>
2013-11-04 16:02:38 -0500

    Building tests for JSON parsing

---
+++ src/gameEngine/parser/Parser.java
@@ -0,0 +1,12 @@
+package gameEngine.parser;
+
+import java.util.Scanner;
+
+public class Parser {
+	private Scanner scanner;
+	public Parser(Scanner scanner) {
+		this.scanner = scanner;
+	}
+	
+	
+}
---
+++ src/gameEngine/test/TestParser.java
@@ -0,0 +1,30 @@
+package gameEngine.test;
+
+import static org.junit.Assert.*;
+import gameEngine.*
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestParser {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCreateParser() {
+		
+	}
+
+}
