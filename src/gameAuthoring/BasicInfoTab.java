5d2782c848a7c6e1aa08c80a882dd70271680663
Susan Zhang <sz56@duke.edu>
2013-11-04 15:05:54 -0500

    Fixed errors

--- src/gameAuthoring/BasicInfoTab.java
+++ src/gameAuthoring/BasicInfoTab.java
@@ -5,6 +5,11 @@ import javax.swing.JPanel;
 
 public class BasicInfoTab extends Tab {
 
+    public BasicInfoTab (GameData gameData) {
+        super(gameData);
+        // TODO Auto-generated constructor stub
+    }
+
     // TO DO: Get rid of magic number
     @Override
     public JPanel getTab () {
