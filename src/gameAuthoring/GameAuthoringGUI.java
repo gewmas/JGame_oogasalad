d7a6a3c9f3d95163face8a15194c6f9796fa2843
BecLai <becdotlai@gmail.com>
2013-11-04 09:01:41 -0500

    Set up basic tab system

--- src/gameAuthoring/GameAuthoringGUI.java
+++ src/gameAuthoring/GameAuthoringGUI.java
@@ -1,5 +1,33 @@
 package gameAuthoring;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+
 public class GameAuthoringGUI {
 
+    public GameAuthoringGUI () {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel();
+        JTabbedPane basicInfoTab = new BasicInfoTab().getTab();
+        JTabbedPane mapDesignTab = new MapDesignTab().getTab();
+        JTabbedPane towerDesignTab = new TowerDesignTab().getTab();
+        JTabbedPane enemyDesignTab = new EnemyDesignTab().getTab();
+        JTabbedPane levelDesignTab = new LevelDesignTab().getTab();
+        mainPanel.add(basicInfoTab);
+        mainPanel.add(mapDesignTab);
+        mainPanel.add(towerDesignTab);
+        mainPanel.add(enemyDesignTab);
+        mainPanel.add(levelDesignTab);
+        frame.setContentPane(mainPanel);
+        frame.pack();
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main (String[] arg) {
+        GameAuthoringGUI gameAuthoringGUI = new GameAuthoringGUI();
+    }
 }
--- src/gameAuthoring/Tab.java
+++ src/gameAuthoring/Tab.java
@@ -1,5 +1,16 @@
 package gameAuthoring;
 
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+
 public class Tab {
+
+    public JTabbedPane getTab () {
+        JTabbedPane tab = new JTabbedPane();
+        JPanel tabPanel = new JPanel();
+        tab.addTab("tab", tabPanel);
+        return tab;
+    }
 
 }
