3954c22dea02346f6e6e33a28ad87f4f134daff8
riadix <alexzzhu@gmail.com>
2013-11-04 20:14:53 -0500

    Removed reference to swt package, was causing swing components to not load

--- src/gameEngine/view/View.java
+++ src/gameEngine/view/View.java
@@ -10,7 +10,7 @@ public class View extends Frame{
     public View(){
         super();
         canvasPanel = new CanvasPanel();
-        this.add( canvasPanel);
+        this.add(canvasPanel);
         pack();
         setVisible(true);
     }
