3039457003f7095868a3f6d421832c36ab4a609d
Harris Osserman <harris.osserman@gmail.com>
2013-11-06 20:13:21 -0500

    completed grid factory but having trouble testing it

--- src/gameEngine/test/TestGrid.java
+++ src/gameEngine/test/TestGrid.java
@@ -2,25 +2,22 @@ package gameEngine.test;
 
 import static org.junit.Assert.*;
 import gameEngine.factory.Factory;
-import gameEngine.model.GameInfo;
 import gameEngine.parser.Parser;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import jgame.JGColor;
+import jgame.platform.JGEngine;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestGrid {
+public class TestGrid extends JGEngine{
 
     @Before
     public void setUp () throws Exception {
-        File file = new File("src/gameEngine/test/testTowerEnemyBullet/mygame.json");
-        Scanner scanner = new Scanner(file);
-        Parser parser = new Parser(scanner);
-        GameInfo gameinfo = new GameInfo(1,2,3, null);
-//        Level level = new Level(gameinfo, parser);
-//        Factory factory = new Factory(parser, level);
-//        factory.grid().initialize();
+        initGame();
+        initCanvas();
         
     }
 
@@ -31,6 +28,47 @@ public class TestGrid {
     @Test
     public void test () {
         fail("Not yet implemented");
+    }
+    
+    @Override
+    public void initCanvas () {
+        setCanvasSettings(
+                          1, // width of the canvas in tiles
+                          1, // height of the canvas in tiles
+                          displayWidth(), // width of one tile
+                          displayHeight(), // height of one tile
+                          null,// foreground colour -> use default colour white
+                          JGColor.white,// background colour -> use default colour black
+                          null // standard font -> use default font
+        );
+    }
+
+    @Override
+    public void initGame () {
+//        defineMedia("mygame.tbl");
+        setFrameRate(60, 2);
+
+        try {
+            File file = new File("src/gameEngine/test/testTowerEnemyBullet/mygame.json");
+            Scanner scanner = new Scanner(file);
+            Parser parser = new Parser(scanner);
+            Factory factory = new Factory(parser);
+            factory.grid().initialize();
+        }
+        catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+
+    @Override
+    public void doFrame () {
+        moveObjects();
+    }
+
+    @Override
+    public void paintFrame () {
     }
 
 }
