c89bfcb6582093ff3f2aa08ed269bb6f636c2321
BecLai <becdotlai@gmail.com>
2013-11-08 09:10:11 -0500

    Added Readable and Writeable interfaces

--- src/gameEngine/model/Enemy.java
+++ src/gameEngine/model/Enemy.java
@@ -1,9 +1,10 @@
 package gameEngine.model;
 
+import java.io.File;
 import jgame.JGObject;
 
 
-public class Enemy extends JGObject {
+public class Enemy extends JGObject implements Readable, Writeable {
 
     public Enemy (String name,
                   boolean unique_id,
@@ -13,6 +14,18 @@ public class Enemy extends JGObject {
                   String gfxname) {
         super(name, unique_id, x, y, collisionid, gfxname);
         // XXX Auto-generated constructor stub
+    }
+
+    @Override
+    public File write () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void read () {
+        // TODO Auto-generated method stub
+
     }
 
 }
