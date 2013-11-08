120d0850f742eec11c798cdf3808ce9301811e81
Harris Osserman <harris.osserman@gmail.com>
2013-11-08 16:13:36 -0500

    changed grid initialization to make linked list of path

--- src/gameEngine/model/Grid.java
+++ src/gameEngine/model/Grid.java
@@ -11,9 +11,11 @@ import jgame.JGObject;
 public class Grid extends JGObject {
     
     private boolean onPath;
+    
     public Grid (String name, boolean unique_id, double x, double y, int collisionid, String gfxname, int tilebbox_x, int tilebbox_y, int tilebbox_width, int tilebbox_height) {
         super(name, unique_id, x, y, collisionid, gfxname, tilebbox_x, tilebbox_y, tilebbox_width, tilebbox_height);
         onPath = false;
+        
     }
     
     public void setOnPath() {
