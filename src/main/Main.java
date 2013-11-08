d35a465ee3752f1d2e983acf682e0e2b1dab369f
BecLai <becdotlai@gmail.com>
2013-11-08 09:01:47 -0500

    Trying to fix merge conflicts

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,12 +2,9 @@ package main;
 
 import gameEngine.view.View;
 
+
 public class Main {
     public static void main (String[] args) {
-<<<<<<< HEAD
-
-=======
         new View();
->>>>>>> 20bd0cd32f5390e67634e3b60d004feac3135f87
     }
 }
