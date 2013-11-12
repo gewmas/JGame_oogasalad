81909c2ba80de6389825d2cffbc2cdf6df9f50cf
riadix <alexzzhu@gmail.com>
2013-11-12 01:20:29 -0500

    Fixed merge conflict with engineFE

--- src/gameEngine/factory/GridFactory.java
+++ src/gameEngine/factory/GridFactory.java
@@ -13,122 +13,72 @@ import gameEngine.parser.JSONLibrary.JSONArray;
 import gameEngine.parser.JSONLibrary.JSONObject;
 import helpers.Coordinate;
 import jgame.platform.*;
-
-
 /**
  * 
  * @author Harris
  * 
- *         Constructs the grid
- * 
+ * Constructs the grid
+ *
  */
 public class GridFactory implements FactoryInterface {
-    private Parser parser;
-    private ArrayList<ArrayList<Tile>> gridList;
-    LinkedList<Tile> path;
-
-<<<<<<< HEAD
-    public GridFactory (Parser parser) {
+        private Parser parser;
+        private ArrayList<ArrayList<Tile>> gridList;
+        LinkedList<Tile> path;
+        public GridFactory(Parser parser) {
         this.parser = parser;
         gridList = new ArrayList<ArrayList<Tile>>();
         path = new LinkedList<Tile>();
-    }
-=======
-	    //get all of the necessary values from the JSON
-	    int height = parser.getInt("heightOfWindow");
-	    int width = parser.getInt("widthOfWindow");
-	    int tilesPerRow = parser.getInt("tilesPerRow");
-	    JSONObject map = parser.getJSONObject("map");
-	    String pathImage = map.getString("pathImage");
-	    JSONArray pathList = map.getJSONArray("Path");
-	    
-	    //create a map of all of the path coordinates and null grid objects
-	    HashMap<Coordinate, Tile> pathCoordinates = new HashMap<Coordinate, Tile>();
-	    for(int k=0; k<pathList.length(); k++) {
-	        JSONObject coord = pathList.getJSONObject(k);
-	        pathCoordinates.put(new Coordinate(coord.getInt("x"), coord.getInt("y")), null);
-	    }
-	    
-	    //create a 2D array of grid elements
-	    
-	    int currentXOffset = 0;
-	    for(int k=0; k<tilesPerRow; k++) {
-	        int currentYOffset = 0;  
-	        gridList.add(new ArrayList<Tile>());
-	        for(int m=0; m<tilesPerRow; m++) {
-	            Tile tile = new Tile(currentXOffset, currentYOffset, (width / tilesPerRow) + currentXOffset, (height / tilesPerRow) + currentYOffset);
-	            gridList.get(k).add(tile);
-	            currentYOffset = currentYOffset + height / tilesPerRow;
-	            if(pathCoordinates.keySet().contains(new Coordinate(k, m))) {
-	                tile.setOnPath(pathImage);
-	                pathCoordinates.put(new Coordinate(k, m), tile);
-	            }
-	        }
-	        currentXOffset = currentXOffset + width / tilesPerRow;
-	    }
-	    
-	   //generate path Linked List
-	    for(int k=0; k<pathList.length(); k++) {
-	        JSONObject coord = pathList.getJSONObject(k);
-	        path.add(pathCoordinates.get(new Coordinate(coord.getInt("x"), coord.getInt("y"))));
-	    }
-	    	    
-	}
->>>>>>> b621795bb87579578567c84e276ddca558ecc5a5
-
-    @Override
-    public void initialize () {
-
-        // get all of the necessary values from the JSON
-        int height = parser.getInt("heightOfWindow");
-        int width = parser.getInt("widthOfWindow");
-        int tilesPerRow = parser.getInt("tilesPerRow");
-        JSONObject map = parser.getJSONObject("map");
-        String pathImage = map.getString("pathImage");
-        JSONArray pathList = map.getJSONArray("Path");
-
-        // create a map of all of the path coordinates and null grid objects
-        HashMap<Coordinate, Tile> pathCoordinates = new HashMap<Coordinate, Tile>();
-        for (int k = 0; k < pathList.length(); k++) {
-            JSONObject coord = pathList.getJSONObject(k);
-            pathCoordinates.put(new Coordinate(coord.getInt("x"), coord.getInt("y")), null);
         }
+        
+        @Override
+        public void initialize() {
 
-        // create a 2D array of grid elements
-        int currentYOffset = 0;
-        for (int k = 0; k < tilesPerRow; k++) {
-            int currentXOffset = 0;
-            gridList.add(new ArrayList<Tile>());
-            for (int m = 0; m < tilesPerRow; m++) {
-                Tile tile =
-                        new Tile(currentXOffset, currentYOffset, (width / tilesPerRow) +
-                                                                 currentXOffset,
-                                 (height / tilesPerRow) + currentYOffset);
-                gridList.get(k).add(tile);
-                currentXOffset = currentXOffset + width / tilesPerRow;
-                if (pathCoordinates.keySet().contains(new Coordinate(k, m))) {
-                    tile.setOnPath(pathImage);
-                    pathCoordinates.put(new Coordinate(k, m), tile);
-                    System.out.println("center x: " + tile.getCenterX() + " and centerY: " +
-                                       tile.getCenterY());
-                }
+            //get all of the necessary values from the JSON
+            int height = parser.getInt("heightOfWindow");
+            int width = parser.getInt("widthOfWindow");
+            int tilesPerRow = parser.getInt("tilesPerRow");
+            JSONObject map = parser.getJSONObject("map");
+            String pathImage = map.getString("pathImage");
+            JSONArray pathList = map.getJSONArray("Path");
+            
+            //create a map of all of the path coordinates and null grid objects
+            HashMap<Coordinate, Tile> pathCoordinates = new HashMap<Coordinate, Tile>();
+            for(int k=0; k<pathList.length(); k++) {
+                JSONObject coord = pathList.getJSONObject(k);
+                pathCoordinates.put(new Coordinate(coord.getInt("x"), coord.getInt("y")), null);
             }
-            currentYOffset = currentYOffset + height / tilesPerRow;
+            
+            //create a 2D array of grid elements
+            
+            int currentXOffset = 0;
+            for(int k=0; k<tilesPerRow; k++) {
+                int currentYOffset = 0;  
+                gridList.add(new ArrayList<Tile>());
+                for(int m=0; m<tilesPerRow; m++) {
+                    Tile tile = new Tile(currentXOffset, currentYOffset, (width / tilesPerRow) + currentXOffset, (height / tilesPerRow) + currentYOffset);
+                    gridList.get(k).add(tile);
+                    currentYOffset = currentYOffset + height / tilesPerRow;
+                    if(pathCoordinates.keySet().contains(new Coordinate(k, m))) {
+                        tile.setOnPath(pathImage);
+                        pathCoordinates.put(new Coordinate(k, m), tile);
+                    }
+                }
+                currentXOffset = currentXOffset + width / tilesPerRow;
+            }
+            
+           //generate path Linked List
+            for(int k=0; k<pathList.length(); k++) {
+                JSONObject coord = pathList.getJSONObject(k);
+                path.add(pathCoordinates.get(new Coordinate(coord.getInt("x"), coord.getInt("y"))));
+            }
+                        
         }
 
-        // generate path Linked List
-        for (int k = 0; k < pathList.length(); k++) {
-            JSONObject coord = pathList.getJSONObject(k);
-            path.add(pathCoordinates.get(new Coordinate(coord.getInt("x"), coord.getInt("y"))));
+        public ArrayList<ArrayList<Tile>> getGridList() {
+            return gridList;
         }
-
-    }
-
-    public ArrayList<ArrayList<Tile>> getGridList () {
-        return gridList;
-    }
-
-    public LinkedList<Tile> getPathList () {
-        return path;
-    }
+        
+        public LinkedList<Tile> getPathList() {
+            return path;
+        }
 }
