8a88a366a2f81f1a9aa7be2f04f4443b77cf4c15
Lalita Maraj <lalita.maraj@duke.edu>
2013-11-17 17:04:06 -0500

    Organized labels.properties file

--- src/gameEngine/view/gameFrame/store/TowerInfoFields.java
+++ src/gameEngine/view/gameFrame/store/TowerInfoFields.java
@@ -1,7 +1,9 @@
 package gameEngine.view.gameFrame.store;
 
+import gameEngine.view.StyleConstants;
+
 /**
- * An enumerated class that contains the Fields of tower information
+ * An interface that contains the Fields of tower information
  * to be displayd in the infoPanel. The fields are returened with HTML formatting
  * 
  * @author Lalita Maraj
@@ -13,15 +15,15 @@ public interface TowerInfoFields {
 
     static final String HTML_FONT_OPENNING = "<li><font color=blue style=font-weight:bold>";
 
-    static final String TOWER = HTML_FONT_OPENNING + "Tower" + HTML_CLOSE;
-    static final String COST = HTML_FONT_OPENNING + "Cost" + HTML_CLOSE;
-    static final String DESCRIPTION = HTML_FONT_OPENNING + "Description" + HTML_CLOSE;
-    static final String ATTACKSPEED = HTML_FONT_OPENNING + "Attack Speed" + HTML_CLOSE;
-    static final String ATTACKMODE = HTML_FONT_OPENNING + "Attack Mode" + HTML_CLOSE;
+    static final String TOWER = HTML_FONT_OPENNING + StyleConstants.resourceBundle.getString("Tower") + HTML_CLOSE;
+    static final String COST = HTML_FONT_OPENNING +  StyleConstants.resourceBundle.getString("Cost") + HTML_CLOSE;
+    static final String DESCRIPTION = HTML_FONT_OPENNING +  StyleConstants.resourceBundle.getString("Description") + HTML_CLOSE;
+    static final String ATTACKSPEED = HTML_FONT_OPENNING +  StyleConstants.resourceBundle.getString("AttackSpeed") + HTML_CLOSE;
+    static final String ATTACKMODE = HTML_FONT_OPENNING +  StyleConstants.resourceBundle.getString("AttackMode") + HTML_CLOSE;
 
-    static final String RANGE = HTML_FONT_OPENNING + "Range" + HTML_CLOSE;
-    static final String RECYCLE = HTML_FONT_OPENNING + "Recycle Price" + HTML_CLOSE;
+    static final String RANGE = HTML_FONT_OPENNING +  StyleConstants.resourceBundle.getString("Range") + HTML_CLOSE;
+    static final String RECYCLE = HTML_FONT_OPENNING +  StyleConstants.resourceBundle.getString("Recycle") + HTML_CLOSE;
 
-    static final String DAMAGE = HTML_FONT_OPENNING + "Damage" + HTML_CLOSE;
+    static final String DAMAGE = HTML_FONT_OPENNING +  StyleConstants.resourceBundle.getString("Damage") + HTML_CLOSE;
 
 }
