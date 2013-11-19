e087da664506f1d4696c7f31dfcbfc7ab965140d
Lalita Maraj <lalita.maraj@duke.edu>
2013-11-19 01:02:00 -0500

    formatted class

--- src/gameEngine/view/gameFrame/cheatCode/CheatCodeFrame.java
+++ src/gameEngine/view/gameFrame/cheatCode/CheatCodeFrame.java
@@ -10,36 +10,37 @@ import javax.swing.JTextArea;
 import javax.swing.table.DefaultTableModel;
 import net.miginfocom.swing.MigLayout;
 
+
 public class CheatCodeFrame extends JFrame {
 
-    CheatCodeFrame(){
+    CheatCodeFrame () {
         super();
-        
+
         MigLayout layout = new MigLayout("wrap 4");
-   
+
         JPanel main = new JPanel(layout);
-        
+
         JLabel available = new JLabel("cheat codes yo");
         JTextArea input = new JTextArea("dfd");
-        input.setPreferredSize(new Dimension(100,100));
+        input.setPreferredSize(new Dimension(100, 100));
         DefaultTableModel model =
                 new DefaultTableModel(null,
                                       new Object[] { "Cheat code", "Success" });
         JTable history = new JTable(model);
-        
-        main.add(history,"span 4");
-        main.add(new JScrollPane(input),"span 3");
-        main.add(available,"span 1");
-        
+
+        main.add(history, "span 4");
+        main.add(new JScrollPane(input), "span 3");
+        main.add(available, "span 1");
+
         add(main);
     }
-    
-    public void showFrame(){
+
+    public void showFrame () {
         pack();
         setVisible(true);
     }
-    
-    public static void main(String[] args){
+
+    public static void main (String[] args) {
         CheatCodeFrame frame = new CheatCodeFrame();
         frame.showFrame();
     }
