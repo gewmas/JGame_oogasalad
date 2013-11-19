9812a17c83a34729333cd077c2e61ae4269e6cd9
Yuhua Mai <yuhua.mai@duke.edu>
2013-11-18 21:10:47 -0500

    Reflection read json in TowerWarehouse, Four types of tower in officialJSON.json

--- src/gameEngine/model/buff/IBuffable.java
+++ src/gameEngine/model/buff/IBuffable.java
@@ -3,14 +3,14 @@ package gameEngine.model.buff;
  * wenxin shi
  */
 public interface IBuffable {
-public double getX();
-public double getY();
-public int getImmuneBuffs();
-public int getAttackBuffs();
-public int getCurrentBuffs();
-public void changeCurrentBuffs(int buff);
-public void changeLife(double lifePercent);
-public void changeSpeed(double speedPercent);
-public void changeAttack(double attackPercent);
-//public void changeAttackSpeed(double offSet);
+    public double getX();
+    public double getY();
+    public int getImmuneBuffs();
+    public int getAttackBuffs();
+    public int getCurrentBuffs();
+    public void changeCurrentBuffs(int buff);
+    public void changeLife(double lifePercent);
+    public void changeSpeed(double speedPercent);
+    public void changeAttack(double attackPercent);
+    //public void changeAttackSpeed(double offSet);
 }
--- src/gameEngine/model/tower/MultipleShootingTower.java
+++ src/gameEngine/model/tower/MultipleShootingTower.java
@@ -2,20 +2,35 @@ package gameEngine.model.tower;
 
 public class MultipleShootingTower extends DefaultTower {
 
+    
+    
     public MultipleShootingTower (double damage,
                                   double attackSpeed,
                                   int attackMode,
+                                  int attackAmount,
                                   double range,
                                   double cost,
                                   double recyclePrice,
+                                  String description,
+                                  
                                   String id,
                                   boolean unique_id,
                                   double x,
                                   double y,
                                   int collisionid,
                                   String image) {
-        super(damage, attackSpeed, attackMode, range, cost, recyclePrice, id, unique_id, x, y, collisionid,
-              image);
+        super(damage, attackSpeed, attackMode, range, cost, recyclePrice, description, id, unique_id, x, y,
+              collisionid, image);
+        
+        this.attackAmount = attackAmount;
+        
+        addDescription();
+    }
+
+    public void addDescription(){
+        super.addDescription();
+        
+        info.put("Attack Amount", String.valueOf(attackAmount));
     }
 
 }
