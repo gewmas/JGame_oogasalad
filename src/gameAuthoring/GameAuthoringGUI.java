0b866a435193e8c90df9d8aac64350e7f2609754
Lalita Maraj <maraj.lalita@gmail.com>
2013-11-21 18:48:37 -0500

    Added a simmulate tab to game authoring gui
    NEED TO JAVADOC!

--- src/gameAuthoring/GameAuthoringGUI.java
+++ src/gameAuthoring/GameAuthoringGUI.java
@@ -14,6 +14,7 @@ public class GameAuthoringGUI {
     private EnemyDesignTab myEnemyDesignTab;
     private LevelDesignTab myLevelDesignTab;
     private MiscellaneousTab myMiscellaneousTab;
+    private SimmulateTab mySimmulationTab;
     
     // TO DO: Get rid of magic numbers
     public GameAuthoringGUI () {
@@ -29,12 +30,14 @@ public class GameAuthoringGUI {
         myEnemyDesignTab = new EnemyDesignTab(gameData);
         myLevelDesignTab = new LevelDesignTab(gameData);
         myMiscellaneousTab = new MiscellaneousTab(gameData);
+        mySimmulationTab = new SimmulateTab(gameData);
         mainPane.addTab("Basic Info", myBasicInfoTab.getTab());
         mainPane.addTab("Map Design", myMapDesignTab.getTab());
         mainPane.addTab("Tower Design", myTowerDesignTab.getTab());
         mainPane.addTab("Enemy Design", myEnemyDesignTab.getTab());
         mainPane.addTab("Level Design", myLevelDesignTab.getTab());
         mainPane.addTab("Miscellaneous", myMiscellaneousTab.getTab());
+        mainPane.addTab("Simmulate", mySimmulationTab.getTab());
         MenuBar menu = new MenuBar(gameData, myBasicInfoTab, myMapDesignTab);
         frame.setJMenuBar(menu);
         frame.setContentPane(mainPane);
---
+++ src/gameAuthoring/SimmulateTab.java
@@ -0,0 +1,18 @@
+package gameAuthoring;
+
+import gameEngine.parser.Parser;
+
+public class SimmulateTab extends Tab {
+
+    public SimmulateTab (GameData gameData) {
+        super(gameData);
+        
+    }
+
+    @Override
+    public void loadJSON (Parser p) {
+        
+
+    }
+
+}
