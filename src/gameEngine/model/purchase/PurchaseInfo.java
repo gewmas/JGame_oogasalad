ac8ca4025f9fca1c86cd58538ef87bc6ec4433cb
Harris Osserman <harris.osserman@gmail.com>
2013-11-24 15:13:17 -0500

    refactor to purchase info

--- src/gameEngine/model/purchase/PurchaseInfo.java
+++ src/gameEngine/model/purchase/PurchaseInfo.java
@@ -1,16 +1,45 @@
 package gameEngine.model.purchase;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * @author Jiaran and Yuhua all tower and tower factory
+ * @author Jiaran, Yuhua, Harris all tower and tower factory
  * implements TowerInfo so that front end people can 
  * get tower info properly without exposing tower and 
  * towerfactory.
  *
  */
-public interface PurchaseInfo {
-    public Map<String, String> getInfo();
-    public String getObjectType();
-    public String getName();
+public abstract class PurchaseInfo {
+    private String name, image, type;
+    private String description;
+    private Integer cost;
+    private Map<String, String> info;
+    public PurchaseInfo(String type, String name, String image, String description, int cost) {
+        this.type = type;
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.cost = cost;
+        info = new LinkedHashMap<String, String>();
+        createMap();
+    }
+    
+    public void createMap() {
+        addToMap("Type", type);
+        addToMap("Name", name);
+        addToMap("Description", description);
+        addToMap("Cost", cost.toString());
+        addToMap("Image", image);
+    }
+    
+    public void addToMap(String key, String value) {
+        info.put(key, value);
+    }
+    
+    public Map<String, String> getInfo() {
+        return info;
+    }
+    
 }
