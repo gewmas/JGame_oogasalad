092898f8a462601665bdc8beab19994589a77b64
Lalita Maraj <maraj.lalita@gmail.com>
2013-11-24 11:39:39 -0500

    Revert 250d148..52004be
    This rolls back to commit 250d14806b27ebca5acfac03edb3319eb0fe12d2.

--- src/gameAuthoring/menuBar/MenuBar.java
+++ src/gameAuthoring/menuBar/MenuBar.java
@@ -63,13 +63,7 @@ public class MenuBar extends JMenuBar {
 
             }
         });
-        menu.add(new AbstractAction("Simulate") {
-            @Override
-            public void actionPerformed (ActionEvent e) {
-                
 
-            }
-        });
         return menu;
     }
 }
