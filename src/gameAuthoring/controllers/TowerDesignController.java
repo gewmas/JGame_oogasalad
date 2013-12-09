bc0b85e14429102cff76715b1819976bfcb14cec
Susan Zhang <sz56@duke.edu>
2013-12-08 23:49:12 -0500

    Setting up controllers for basic, tempbarrier, and tower design

---
+++ src/gameAuthoring/controllers/TowerDesignController.java
@@ -0,0 +1,21 @@
+package gameAuthoring.controllers;
+
+import java.util.Observable;
+import gameAuthoring.JSONObjects.GameData;
+import gameAuthoring.JSONObjects.TowerJSONObject;
+
+public class TowerDesignController extends DesignController {
+
+    public TowerDesignController (GameData gameData) {
+        super(gameData);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update (Observable o, Object arg) {
+        System.out.println("TowerDesignController received update from EnemyDesignTab");
+        TowerJSONObject tower = (TowerJSONObject) arg;
+        myGameData.addTower(tower);
+    }
+
+}
