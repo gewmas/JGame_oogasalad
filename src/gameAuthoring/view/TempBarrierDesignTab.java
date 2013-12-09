31f97cdf0d27b9c64f51544c22de6927155d3201
BecLai <becdotlai@gmail.com>
2013-12-09 05:01:34 -0500

    TemporaryBarrierTab refactored

--- src/gameAuthoring/view/TempBarrierDesignTab.java
+++ src/gameAuthoring/view/TempBarrierDesignTab.java
@@ -2,9 +2,7 @@ package gameAuthoring.view;
 
 import gameAuthoring.JSONObjects.TemporaryBarrierJSONObject;
 import gameEngine.parser.Parser;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -20,11 +18,15 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import net.miginfocom.swing.MigLayout;
 
 
+/**
+ * @author Rebecca Lai & Susan Zhang
+ *         Tab that allows for design of temporary barriers in game
+ *         Name, damage, cost, expiration, description, and image can be set per barrier
+ */
 public class TempBarrierDesignTab extends Tab {
 
     private JScrollPane myCreatedTempBarriers;
@@ -37,6 +39,7 @@ public class TempBarrierDesignTab extends Tab {
     private JTextArea myDescriptionField;
     private ImageLabel myBarrierImage;
     private static final Dimension PANEL_DIMENSION = new Dimension(380, 350);
+    private static final Dimension TEXT_FIELD_DIMENSION = new Dimension(200, 30);
     private static final String SCROLL_PANEL_WRAP_MODE = "wrap 4";
 
     public TempBarrierDesignTab () {
@@ -74,69 +77,99 @@ public class TempBarrierDesignTab extends Tab {
 
     }
 
-    public void addCreatedBarriers () {
+    /**
+     * Add ScrollPanel to display created barriers
+     */
+    private void addCreatedBarriers () {
         myCreatedTempBarriers = new JScrollPane(myScrollPanel);
         myCreatedTempBarriers.getViewport().setOpaque(false);
         myCreatedTempBarriers.setOpaque(false);
-        myCreatedTempBarriers.setPreferredSize(new Dimension(380, 350));
-        Border b = BorderFactory.createLineBorder(Color.black, 1);
+        myCreatedTempBarriers.setPreferredSize(PANEL_DIMENSION);
         myCreatedTempBarriers.setBorder(BorderFactory
-                .createTitledBorder(b, "Created Waves",
+                .createTitledBorder(StyleConstants.DEFAULT_PANEL_BORDER,
+                                    StyleConstants.resourceBundle.getString("CreatedBarriers"),
                                     TitledBorder.CENTER,
                                     TitledBorder.TOP,
-                                    new Font("Calibri", Font.PLAIN, 20)));
+                                    StyleConstants.TITLE_FONT_2));
     }
 
-    public void addBarrierCreationButton () {
-        JButton createBarrierButton = new JButton("Create New Barrier");
+    /**
+     * Add button that allows for creation of new barrier
+     */
+    private void addBarrierCreationButton () {
+        JButton createBarrierButton =
+                new JButton(StyleConstants.resourceBundle.getString("NewBarrier"));
         createBarrierButton.setFont(StyleConstants.DEFAULT_BODY_FONT);
         createBarrierButton.addMouseListener(createBarrierListener());
+        createBarrierButton
+                .setToolTipText(StyleConstants.resourceBundle.getString("NewBarrierTip"));
         myContentPanel.add(createBarrierButton);
     }
 
-    public void addBarrierName () {
-        JLabel name = new JLabel("Name");
+    /**
+     * Add text field to allow user input for barrier name
+     */
+    private void addBarrierName () {
+        JLabel name = new JLabel(StyleConstants.resourceBundle.getString("BarrierName"));
         name.setFont(StyleConstants.DEFAULT_BODY_FONT);
+        name.setToolTipText(StyleConstants.resourceBundle.getString("BarrierNameTip"));
         myNameField = new JTextField();
-        myNameField.setPreferredSize(new Dimension(200, 30));
+        myNameField.setPreferredSize(TEXT_FIELD_DIMENSION);
         myNameField.setFont(StyleConstants.DEFAULT_BODY_FONT);
         myContentPanel.add(name);
         myContentPanel.add(myNameField);
     }
 
-    public void addBarrierDamage () {
-        JLabel damage = new JLabel("Damage");
+    /**
+     * Add text field to allow user input for barrier damage
+     */
+    private void addBarrierDamage () {
+        JLabel damage = new JLabel(StyleConstants.resourceBundle.getString("BarrierDamage"));
         damage.setFont(StyleConstants.DEFAULT_BODY_FONT);
+        damage.setToolTipText(StyleConstants.resourceBundle.getString("BarrierDamageTip"));
         myDamageField = new JTextField();
-        myDamageField.setPreferredSize(new Dimension(200, 30));
+        myDamageField.setPreferredSize(TEXT_FIELD_DIMENSION);
         myDamageField.setFont(StyleConstants.DEFAULT_BODY_FONT);
         myContentPanel.add(damage);
         myContentPanel.add(myDamageField);
     }
 
-    public void addBarrierCost () {
-        JLabel cost = new JLabel("Cost");
+    /**
+     * Add text field to allow user input for barrier cost
+     */
+    private void addBarrierCost () {
+        JLabel cost = new JLabel(StyleConstants.resourceBundle.getString("BarrierCost"));
         cost.setFont(StyleConstants.DEFAULT_BODY_FONT);
+        cost.setToolTipText(StyleConstants.resourceBundle.getString("BarrierCostTip"));
         myCostField = new JTextField();
-        myCostField.setPreferredSize(new Dimension(200, 30));
+        myCostField.setPreferredSize(TEXT_FIELD_DIMENSION);
         myCostField.setFont(StyleConstants.DEFAULT_BODY_FONT);
         myContentPanel.add(cost);
         myContentPanel.add(myCostField);
     }
 
-    public void addBarrierExpiry () {
-        JLabel expire = new JLabel("Expire");
+    /**
+     * Add text field to allow user input for barrier expiration time
+     */
+    private void addBarrierExpiry () {
+        JLabel expire = new JLabel(StyleConstants.resourceBundle.getString("BarrierExpiry"));
         expire.setFont(StyleConstants.DEFAULT_BODY_FONT);
+        expire.setToolTipText(StyleConstants.resourceBundle.getString("BarrierExpiryTip"));
         myExpiryField = new JTextField();
-        myExpiryField.setPreferredSize(new Dimension(200, 30));
+        myExpiryField.setPreferredSize(TEXT_FIELD_DIMENSION);
         myExpiryField.setFont(StyleConstants.DEFAULT_BODY_FONT);
         myContentPanel.add(expire);
         myContentPanel.add(myExpiryField);
     }
 
-    public void addBarrierDescription () {
-        JLabel description = new JLabel("Description");
+    /**
+     * Add text area to allow user input for barrier description
+     */
+    private void addBarrierDescription () {
+        JLabel description =
+                new JLabel(StyleConstants.resourceBundle.getString("BarrierDescription"));
         description.setFont(StyleConstants.DEFAULT_BODY_FONT);
+        description.setToolTipText(StyleConstants.resourceBundle.getString("BarrierExpiryTip"));
         myDescriptionField = new JTextArea(2, 15);
         myDescriptionField.setLineWrap(true);
         JScrollPane scrollPane = new JScrollPane(myDescriptionField);
@@ -144,15 +177,25 @@ public class TempBarrierDesignTab extends Tab {
         myContentPanel.add(scrollPane);
     }
 
-    public void addBarrierCreationImage () {
-        JLabel image = new JLabel("Choose image");
+    /**
+     * Add image label for barrier image
+     */
+    private void addBarrierCreationImage () {
+        JLabel image = new JLabel(StyleConstants.resourceBundle.getString("BarrierSelectImage"));
         image.setFont(StyleConstants.DEFAULT_BODY_FONT);
+        image.setToolTipText(StyleConstants.resourceBundle.getString("BarrierSelectImageTip"));
         myBarrierImage = new ImageLabel();
         myContentPanel.add(image);
         myContentPanel.add(myBarrierImage);
     }
 
-    protected void addBarrier (File imgSource, String BarrierName) {
+    /**
+     * Add barrier icon to ScrollPanel once barrier has been created
+     * 
+     * @param imgSource is image File to be used for barrier
+     * @param BarrierName is image path of image
+     */
+    private void addBarrier (File imgSource, String BarrierName) {
         JLabel barrierIcon = new JLabel();
         try {
             Image barrierImage = ImageIO.read(imgSource);
@@ -165,21 +208,21 @@ public class TempBarrierDesignTab extends Tab {
         JLabel barrierNameLabel = new JLabel(BarrierName);
         myScrollPanel.add(barrierNameLabel);
         myScrollPanel.add(barrierIcon);
-
     }
 
-    public MouseAdapter createBarrierListener () {
+    /**
+     * @return MouseAdapter that allows for barrier to be created once fields are completed
+     */
+    private MouseAdapter createBarrierListener () {
         MouseAdapter listener = new MouseAdapter() {
             @Override
             public void mouseClicked (MouseEvent e) {
-
                 String name = myNameField.getText();
                 int damage = Integer.parseInt(myDamageField.getText());
                 int cost = Integer.parseInt(myCostField.getText());
                 int expire = Integer.parseInt(myExpiryField.getText());
                 String description = myDescriptionField.getText();
                 String image = myBarrierImage.getID();
-
                 if (name != null && description != null && image != null && expire > 0) {
                     TemporaryBarrierJSONObject temporaryBarrier =
                             new TemporaryBarrierJSONObject(name, image, damage, cost, expire,
@@ -194,14 +237,13 @@ public class TempBarrierDesignTab extends Tab {
                     myExpiryField.setText("");
                     myDescriptionField.setText("");
                     myBarrierImage.reset();
-
                 }
                 else {
-                    JOptionPane.showMessageDialog(null, "One or more fields invalid!");
+                    JOptionPane.showMessageDialog(null, StyleConstants.resourceBundle
+                            .getString("BarrierInvalidSubmission"));
                 }
             }
         };
-
         return listener;
     }
 }
