b5073049cd09ce0032ff8c9ebe42a5f341437527
Lalita Maraj <maraj.lalita@gmail.com>
2013-12-08 23:30:54 -0500

    Added javadoc explanation of DisplayValue class

--- src/gameEngine/view/gameFrame/tools/InfoDisplayPanel.java
+++ src/gameEngine/view/gameFrame/tools/InfoDisplayPanel.java
@@ -2,7 +2,6 @@ package gameEngine.view.gameFrame.tools;
 
 import java.awt.Dimension;
 import java.util.Collection;
-import java.util.Map;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
