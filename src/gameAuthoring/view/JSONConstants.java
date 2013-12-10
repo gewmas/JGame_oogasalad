f50020a54c8c0befc3d9e5a216c88c4726e561d0
BecLai <becdotlai@gmail.com>
2013-12-09 23:20:31 -0500

    Refactored loadJSON methods

---
+++ src/gameAuthoring/view/JSONConstants.java
@@ -0,0 +1,24 @@
+package gameAuthoring.view;
+
+/**
+ * @author Rebecca Lai & Susan Zhang
+ *         Constants that apply for ALL Game Authoring classes pertaining to JSON reading and
+ *         writing
+ */
+public final class JSONConstants {
+    public static final String RESOURCE_STRING = "resources";
+    public static final String IMAGE_STRING = "image";
+    public static final String ID_STRING = "id";
+    public static final String URL_STRING = "url";
+    public static final String X_STRING = "x";
+    public static final String Y_STRING = "y";
+    public static final String MAP_STRING = "map";
+    public static final String PATH_STRING = "Path";
+    public static final String SPLASH_STRING = "splashImage";
+    public static final String AUDIO_STRING = "audio";
+    public static final String BULLET_STRING = "bullet";
+    public static final String BG_AUDIO_STRING = "BGAudio";
+    public static final String TOWER_STRING = "Tower";
+    public static final String NAME_STRING = "Name";
+    public static final String IMAGE_STRING_2 = "Image";
+}
