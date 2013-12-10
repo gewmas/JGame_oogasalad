c973eb12882d35a3c590ec1141b07d1bdb5fa78e
BecLai <becdotlai@gmail.com>
2013-12-10 12:59:53 -0500

    More fixes to DuvallClippy

--- src/gameAuthoring/view/DuvallClippy.java
+++ src/gameAuthoring/view/DuvallClippy.java
@@ -3,18 +3,24 @@ package gameAuthoring.view;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.Timer;
 import net.miginfocom.swing.MigLayout;
 
 
@@ -28,12 +34,15 @@ public class DuvallClippy extends JFrame implements Observer {
     private Map<String, String> myDisplayTips = new HashMap<String, String>();
     private String myDisplayString = StyleConstants.resourceBundle.getString("BasicInfoHelp");
     private JTextArea myDisplayBox;
+    private List<String> myImageStrings = new ArrayList<String>();
     private static final String DEFAULT_BACKGROUND_IMAGE = "glitter.jpg";
     private static final String DEFAULT_WRAP_MODE = "wrap 2";
     private static final Dimension FRAME_DIMENSION = new Dimension(400, 300);
     private static final Dimension CLIPPY_PANEL_DIMENSION = new Dimension(200, 300);
-    private static final String DUVALL_IMAGE = "duvall_clippy.png";
+    private static final String DUVALL_IMAGE_1 = "duvall_clippy_1.png";
+    private static final String DUVALL_IMAGE_2 = "duvall_clippy_2.png";
     private static final Font DEFAULT_FONT = new Font("Arial", Font.BOLD, 16);
+    private JLabel myDuvallClippyImage;
 
     /**
      * Creates new DuvallClippy
@@ -46,16 +55,16 @@ public class DuvallClippy extends JFrame implements Observer {
         JPanel clippyPanel = new JPanel();
         clippyPanel.setPreferredSize(CLIPPY_PANEL_DIMENSION);
         clippyPanel.setOpaque(false);
-        JLabel duvallClippy = new JLabel();
+        myDuvallClippyImage = new JLabel();
         Image duvallImage;
         try {
-            duvallImage = ImageIO.read(this.getClass().getResource(DUVALL_IMAGE));
-            duvallClippy.setIcon(new ImageIcon(duvallImage));
+            duvallImage = ImageIO.read(this.getClass().getResource(DUVALL_IMAGE_1));
+            myDuvallClippyImage.setIcon(new ImageIcon(duvallImage));
         }
         catch (IOException e1) {
             e1.printStackTrace();
         }
-        clippyPanel.add(duvallClippy);
+        clippyPanel.add(myDuvallClippyImage);
         mainPanel.add(clippyPanel);
         myDisplayBox = new JTextArea(myDisplayString);
         myDisplayBox.setFont(DEFAULT_FONT);
@@ -75,6 +84,13 @@ public class DuvallClippy extends JFrame implements Observer {
         this.setVisible(true);
         this.setResizable(false);
         fillDisplayTipsMap();
+        addAnimations();
+        new Timer(15, makeDuvallBlink).start();
+    }
+
+    public void addAnimations () {
+        myImageStrings.add(DUVALL_IMAGE_1);
+        myImageStrings.add(DUVALL_IMAGE_2);
     }
 
     /*
@@ -88,6 +104,26 @@ public class DuvallClippy extends JFrame implements Observer {
         myDisplayString = myDisplayTips.get(displayKey);
         myDisplayBox.setText(myDisplayString);
     }
+
+    @SuppressWarnings("serial")
+    Action makeDuvallBlink = new AbstractAction() {
+        int myImageIndex = 0;
+
+        @Override
+        public void actionPerformed (ActionEvent arg0) {
+            myImageIndex++;
+            Image duvallImage;
+            int imageIndex = myImageIndex % myImageStrings.size();
+            try {
+                duvallImage =
+                        ImageIO.read(this.getClass().getResource(myImageStrings.get(imageIndex)));
+                myDuvallClippyImage.setIcon(new ImageIcon(duvallImage));
+            }
+            catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    };
 
     /**
      * Create map that allows for DuvallClippy's help tips to be displayed without "if" statements
